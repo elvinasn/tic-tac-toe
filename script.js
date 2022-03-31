e639c65aeedabeb4b946532e3bfc6e174bc888f8
Elvinas <elviz.nejuz@gmail.com>
2022-03-31 08:19:13 +0300

    added that players can see who's turn it is, restart and play again buttons

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
 const body = document.querySelector("body");
+const restart = document.querySelector(".restart");
+
 const Person = (name, sign, turn) => {
   const getName = () => name;
   const getSign = () => sign;
   const getTurn = () => turn;
+  const setTurn = (value) => {
+    turn = value;
+  };
   const changeTurn = () => {
     turn = !turn;
   };
-  return { getName, getSign, getTurn, changeTurn };
+  return { getName, getSign, getTurn, changeTurn, setTurn };
 };
 
 const playerOne = Person("Elvinas", "X", true);
 const playerTwo = Person("Lukas", "O", false);
+
+const GameController = (() => {
+  let previousGameTurn = false;
+  const beginGame = () => {
+    playerOne.setTurn(!previousGameTurn);
+    playerTwo.setTurn(previousGameTurn);
+    GameBoard.clearGameBoard();
+    displayController.updateGameBoard(GameBoard.getGameBoard(), body);
+    displayController.displayTurn(playerOne, playerTwo);
+    displayController.removeResultAndButton();
+    previousGameTurn = !previousGameTurn;
+  };
+
+  const checkIfFinished = () => {
+    if (GameBoard.checkIfWinner()) {
+      displayController.displayResult(
+        false,
+        GameBoard.checkIfWinner().getName()
+      );
+      displayController.removeTurn();
+      endOfGame = true;
+      return true;
+    }
+    if (GameBoard.checkIfDraw()) {
+      displayController.displayResult(true, "");
+      displayController.removeTurn();
+      endOfGame = true;
+      return true;
+    }
+  };
+  return { beginGame, checkIfFinished };
+})();
 
 const GameBoard = (() => {
   let gameboard = [
@@ -18,6 +55,8 @@ const GameBoard = (() => {
     [null, null, null],
     [null, null, null],
   ];
+
+  const isFinished = () => checkIfWinner() || checkIfDraw();
 
   const getGameBoard = () => gameboard;
 
@@ -53,9 +92,11 @@ const GameBoard = (() => {
         gameboard[index][i] !== gameboard[index][i - 1] ||
         gameboard[index][i] === null
       ) {
+        gameFinished = false;
         return false;
       }
     }
+    gameFinished = true;
     return gameboard[index][i - 1];
   };
 
@@ -66,9 +107,11 @@ const GameBoard = (() => {
         gameboard[i][index] !== gameboard[i - 1][index] ||
         gameboard[i][index] === null
       ) {
+        gameFinished = false;
         return false;
       }
     }
+    gameFinished = true;
     return gameboard[i - 1][index];
   };
 
@@ -79,9 +122,11 @@ const GameBoard = (() => {
         gameboard[i][i] !== gameboard[i - 1][i - 1] ||
         gameboard[i][i] === null
       ) {
+        gameFinished = false;
         return false;
       }
     }
+    gameFinished = true;
     return gameboard[i - 1][i - 1];
   };
 
@@ -93,9 +138,11 @@ const GameBoard = (() => {
         gameboard[i][j] !== gameboard[i - 1][j + 1] ||
         gameboard[i][j] === null
       ) {
+        gameFinished = false;
         return false;
       }
     }
+    gameFinished = true;
     return gameboard[i - 1][j + 1];
   };
 
@@ -116,10 +163,18 @@ const GameBoard = (() => {
     checkIfWinner,
     checkIfDraw,
     clearGameBoard,
+    isFinished,
   };
 })();
 const displayController = (() => {
+  const restart = document.createElement("button");
+  restart.textContent = "Play again?";
+  restart.classList.add("restart");
+
   const container = document.createElement("div");
+  const result = document.createElement("p");
+  const turn = document.createElement("p");
+
   const updateGameBoard = (gameBoard) => {
     container.innerHTML = "";
     container.classList.add("gameBoard");
@@ -139,44 +194,81 @@ const displayController = (() => {
     }
     body.appendChild(container);
   };
-  return { updateGameBoard, container };
+
+  const displayResult = (isDraw, name) => {
+    if (isDraw) {
+      result.textContent = "It's a draw!";
+    } else {
+      result.textContent = `${name} has won!`;
+    }
+    body.appendChild(result);
+    body.appendChild(restart);
+  };
+
+  const removeResultAndButton = () => {
+    Array.from(body.childNodes).includes;
+    if (Array.from(body.childNodes).includes(restart)) {
+      body.removeChild(restart);
+    }
+    if (Array.from(body.childNodes).includes(result)) {
+      body.removeChild(result);
+    }
+  };
+
+  const removeTurn = () => {
+    body.removeChild(turn);
+  };
+
+  const displayTurn = (player1, player2) => {
+    const which = player1.getTurn() ? player1 : player2;
+    turn.textContent = `It's ${which.getName()}(${which.getSign()}) turn!`;
+    body.appendChild(turn);
+  };
+
+  return {
+    updateGameBoard,
+    container,
+    displayResult,
+    restart,
+    removeResultAndButton,
+    displayTurn,
+    removeTurn,
+  };
 })();
 
 const EventHandler = (() => {
-  const HandleClick = (e) => {
-    if (e.target.hasAttribute("data-xcoord")) {
+  const HandlePlayerMove = (e) => {
+    if (e.target.hasAttribute("data-xcoord") && !GameBoard.isFinished()) {
       GameBoard.updateGameBoard(
         e.target.dataset.xcoord,
         e.target.dataset.ycoord,
         playerOne.getTurn() ? playerOne : playerTwo
       );
       displayController.updateGameBoard(GameBoard.getGameBoard(), body);
+      if (GameController.checkIfFinished()) {
+        return;
+      }
+
       playerOne.changeTurn();
       playerTwo.changeTurn();
-      if (GameBoard.checkIfWinner()) {
-        const para = document.createElement("p");
-        para.textContent = `${GameBoard.checkIfWinner().getName()} has won!`;
-        body.appendChild(para);
-        GameBoard.clearGameBoard();
-        console.log(GameBoard.getGameBoard());
-        displayController.updateGameBoard(GameBoard.getGameBoard());
-        return;
-      }
-      if (GameBoard.checkIfDraw()) {
-        const para = document.createElement("p");
-        para.textContent = "It's a draw!";
-        body.appendChild(para);
-        GameBoard.clearGameBoard();
-        console.log(GameBoard.getGameBoard());
-        displayController.updateGameBoard(GameBoard.getGameBoard());
-        return;
-      }
+      displayController.displayTurn(playerOne, playerTwo);
     }
   };
-  return { HandleClick };
+
+  const HandleRestartButton = () => {
+    GameController.beginGame();
+  };
+  return { HandlePlayerMove, HandleRestartButton };
 })();
 
-displayController.updateGameBoard(GameBoard.getGameBoard(), body);
 displayController.container.addEventListener("click", (e) =>
-  EventHandler.HandleClick(e)
+  EventHandler.HandlePlayerMove(e)
 );
+displayController.restart.addEventListener("click", () => {
+  EventHandler.HandleRestartButton();
+});
+restart.addEventListener("click", () => {
+  EventHandler.HandleRestartButton();
+});
+
+GameController.beginGame();
